f494ab22a9705cd8f93fe88cbb51f6cfe35de3b5
Zahra seyedi <za.seyyedi@gmail.com>
2019-12-20 17:58:24 +0330

    functional information page

--- src/pages/InformationPage.js
+++ src/pages/InformationPage.js
@@ -1,55 +1,90 @@
 import React from 'react';
 import '../styles/css/informationPage.css'
+
 export default class InformationPage extends React.Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            LastName: '',
+            mobile: '',
+            phone: '',
+            address: '',
+            gender: true
+        };
+    }
+
+    insertKarfarma() {
+        let data = {
+            fistname: this.state.name,
+            LastName: this.state.LastName,
+            mobile: this.state.mobile,
+            phone: this.state.phone,
+            address: this.state.address,
+            gender: this.state.gender === true ? 'man': 'woman',
+        }
+        console.log(data)
+    }
+
+    render() {
         return (
             <div className="App">
-        
-                <form action="http://localhost:3000/" method="post" className="App">
+
+
                 <text readOnly="readonly" disabled="disable" className="txtname">لطفا اطلاعات خود را وارد نمایید</text>
-                 
+
                 <div className="box">
                     <content>نام</content>
-                    <input type="textbox"/>
+                    <input type="textbox"
+                        value={this.state.name}
+                        onChange={(content) => this.setState({ name: content.target.value })} />
                 </div>
-                 
+
                 <div className="box">
                     <content>نام خانوادگی</content>
-                    <input type="textbox"/>
+                    <input type="textbox"
+                        value={this.state.LastName}
+                        onChange={(content) => this.setState({ LastName: content.target.value })} />
                 </div>
-                 
+
                 <div className="box">
                     <content>تلفن همراه</content>
-                    <input type="textbox"/>
+                    <input type="textbox"
+                        value={this.state.mobile}
+                        onChange={(content) => this.setState({ mobile: content.target.value })} />
                 </div>
-                 
+
                 <div className="box">
                     <content>تلفن ثابت</content>
-                    <input type="textbox"/>
+                    <input type="textbox"
+                        value={this.state.phone}
+                        onChange={(content) => this.setState({ phone: content.target.value })} />
                 </div>
-                 
+
                 <div className="line"></div>
-                 
+
                 <div className="boxaddress">
                     <content>آدرس دقیق</content>
-                    <input type="textbox"/>
+                    <input type="textbox"
+                        value={this.state.address}
+                        onChange={(content) => this.setState({ address: content.target.value })} />
                 </div>
-                 
+
                 <div className="gender">
-                  <div>جنسیت</div>
-                  <div className="inGender">
-                  <div className="man">آقا</div>
-                  <div className="woman">خانم</div>
-                  </div>
+                    <div>جنسیت</div>
+                    <div className="inGender">
+                        <button className={this.state.gender === true ? "blue" : "white"} onClick={() => this.setState({ gender: true })}>آقا</button>
+                        <button className={this.state.gender === false ? "blue" : "white"} onClick={() => this.setState({ gender: false })}>خانم</button>
+                    </div>
                 </div>
-                
-                 
+
+
                 <div className="send">
-                <button type="submit">مرحله ی بعد</button>
+                    <button type="submit" onClick={() => this.insertKarfarma()}>مرحله ی بعد</button>
                 </div>
-                </form>
-            
+
+
             </div>
-          );
+        );
     }
-  }
+}
